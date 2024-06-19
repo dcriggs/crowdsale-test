58ee123923707b71f673d336b47ecdff26bc7d61
Dawson Riggs <dawsoncriggs@gmail.com>
2024-06-19 18:09:28 -0500

    Load accounts info

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
+import { ethers } from "ethers";
 
-import Navigation from './Navigation';
+import Navigation from "./Navigation";
+import Info from "./Info";
 
 function App() {
+
+  const [account, setAccount] = useState(null);
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(ethers.utils.getAddress(accounts[0]));
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  });
+
   return (
     <Container>
       <Navigation />
+      <hr />
+      {account && (
+        <Info account={account} />
+      )}      
     </Container>
   );
 }
