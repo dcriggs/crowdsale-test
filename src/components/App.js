44425f6780f2ded8bf83460e18b21a75b9b35c63
Dawson Riggs <dawsoncriggs@gmail.com>
2024-06-19 17:36:51 -0500

    Add website

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+
+import Navigation from './Navigation';
+
+function App() {
+  return (
+    <Container>
+      <Navigation />
+    </Container>
+  );
+}
+
+export default App;
